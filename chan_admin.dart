d7d891c6a752f0c0c073ed3276509e4154547008
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-23 22:03:09 -0500

    Change Channel Admin Config

--- chan_admin.dart
+++ chan_admin.dart
@@ -1,13 +1,15 @@
 part of ghbot;
 
 Future<Map<String, Object>> getChannelInfo(String network, String channel) {
-  return plugin.callMethod(
-      "getChannel", {"network": network, "channel": channel});
+  return plugin.callMethod("getChannel", {
+    "network": network,
+    "channel": channel
+  });
 }
 
 String getUserName(String name) {
   var mapping = config["github"]["users"];
-  
+
   if (mapping != null && mapping.containsKey(name)) {
     return mapping[name];
   } else {
@@ -21,50 +23,51 @@ void syncTeamChannel(String network, String channel) {
   }
 
   Map<String, dynamic> conf = config['github']['channel_admin'];
-  
+
   if (conf == null) {
     return;
   }
-  
-  List<String> channels = conf["in"];
-  int opsTeam = conf["ops_team"];
-  int voicesTeam = conf["voices_team"];
-  
+
   var id = "${network}:${channel}";
-  
-  if (channels.contains(id)) {
-    if (!conf['enabled']) return;
-    
-    bot.getChannel(network, channel).then((info) {
-      github.organizations
-        .listTeamMembers(opsTeam)
-        .toList()
-        .then((members) {
-        for (var member in members) {
-          var name = getUserName(member.login).toLowerCase();
-          var ops = info.ops.map((it) => it.toLowerCase());
-          var members = info.members.map((it) => it.toLowerCase());
-          
-          if (!ops.contains(name) && members.contains(name)) {
-            bot.op(network, channel, name);
-          }
-        }
-      });
-      
-      github.organizations
-        .listTeamMembers(voicesTeam)
-        .toList()
-        .then((members) {
-        for (var member in members) {
-          var name = getUserName(member.login).toLowerCase();
-          var voices = info.voices.map((it) => it.toLowerCase());
-          var members = info.members.map((it) => it.toLowerCase());
-          
-          if (!voices.contains(name) && members.contains(name)) {
-            bot.voice(network, channel, name);
-          }
-        }
-      });
-    });
+  List<Map<String, dynamic>> channels = conf["channels"];
+  if (!conf['enabled']) return;
+
+  if (!channels.any((it) => it["network"] == network && it["channel"] == channel)) {
+    return;
   }
+
+  var c = channels.firstWhere((it) => it["network"] == network && it["channel"] == channel);
+  
+  if (c["enabled"] == false) {
+    return;
+  }
+  
+  var opsTeam = c["ops_team"];
+  var voicesTeam = c["voices_team"];
+  
+  bot.getChannel(network, channel).then((info) {
+    github.organizations.listTeamMembers(opsTeam).toList().then((members) {
+      for (var member in members) {
+        var name = getUserName(member.login).toLowerCase();
+        var ops = info.ops.map((it) => it.toLowerCase());
+        var members = info.members.map((it) => it.toLowerCase());
+
+        if (!ops.contains(name) && members.contains(name)) {
+          bot.op(network, channel, name);
+        }
+      }
+    });
+
+    github.organizations.listTeamMembers(voicesTeam).toList().then((members) {
+      for (var member in members) {
+        var name = getUserName(member.login).toLowerCase();
+        var voices = info.voices.map((it) => it.toLowerCase());
+        var members = info.members.map((it) => it.toLowerCase());
+
+        if (!voices.contains(name) && members.contains(name)) {
+          bot.voice(network, channel, name);
+        }
+      }
+    });
+  });
 }
